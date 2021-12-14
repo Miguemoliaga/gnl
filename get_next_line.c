76c3c65cacd7db534565e4311afa63ce6a778cbd
Miguel Martinez oliaga <mmartine@c3r6s5.42madrid.com>
2021-12-14 20:40:53 +0100

    me falta nada

--- get_next_line.c
+++ get_next_line.c
@@ -11,43 +11,39 @@ char	*multifunctcpy(char *buff, char *str, int start, int end)
 	free(bufsplit);
 	return (str);
 }
-/*
-char	*strtreat(char *line, char *str, int fd)
+
+char	*strtreat(char *str, int fd)
 {
-	int		size;
+	int		lj;
 	int		re;
 	char	*buff;
 
+	lj = -1;
+	re = 1;
 	buff = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
 	if (!buff)
 		return (NULL);
-	size = 1;
-	while (size)
+	re = read(fd, buff, BUFFER_SIZE);
+	while (lj == -1 && re != 0)
 	{
+		lj = search_lj(buff);
+		str = multifunctcpy(buff, str, 0, BUFFER_SIZE);
 		re = read(fd, buff, BUFFER_SIZE);
-		if (re <= 0)
-			return (NULL);
-		size = search_lj(buff);
-		if (size == -1)
-			str = multifunctcpy(buff, str, 0, BUFFER_SIZE);
-		else
-		{
-			str = multifunctcpy(buff, str, 0, size);
-			line = multifunctcpy(buff, line, size + 1, BUFFER_SIZE);
-			printf("line vale %s\n", line);
-			size = 0;
-		}
+		if (re == 0)
+			return (str);
 	}
+	if (re == 0)
+		return (NULL);
 	return (str);
 }
 
 char	*get_next_line(int fd)
 {
 	static char	*line;
-	char		*ret;
+	char		*str;
 	int			linelj;
 
-	ret = "";
+	str = "";
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
 	if (!line)
@@ -55,45 +51,19 @@ char	*get_next_line(int fd)
 	linelj = search_lj(line);
 	if (linelj != -1)
 	{
-		ret = ft_substr(line, 0, linelj);
+		str = ft_substr(line, 0, linelj);
 		line = ft_substr(line, linelj + 1, BUFFER_SIZE);
 	}
 	else
 	{
-		printf("antes de volcar line en ret, line vale: %s\n", line);
-		ret = ft_strdup(line);
-		line = "";
-		printf("despues de volcar  line en ret, ret vale: %s y line vale: %s\n", ret, line);
-		ret = strtreat(line, ret, fd);
+		str = ft_strdup(line);
+		str = strtreat(str, fd);
+		if (!str)
+			return (NULL);
+		line = ft_substr(str, search_lj(str) + 1, BUFFER_SIZE);
+		str = ft_substr(str, 0, search_lj(str));
 	}
-	return (ret);
-}
-*/
-	//problemas al hacer cosas con line, no estamos guardando el valor adecuado.
-
-char	*get_next_line(int fd)
-{
-	static char	*line;
-	char		*ret;
-	int			linelj;
-	int			re;
-
-	ret = "";
-	if (fd < 0 || BUFFER_SIZE <= 0)
-		return (NULL);
-	if (!line)
-		line = "";
-	linelj = search_lj(line);
-	if (linelj != -1)
-	{
-		ret = ft_substr(line, 0, linelj);
-		line = ft_substr(line, linelj + 1, BUFFER_SIZE);
-	}
-	else
-	{
-		
-	}
-	return (ret);
+	return (str);
 }
 
 int	main(void)
@@ -104,7 +74,7 @@ int	main(void)
 
 	i = 0;
 	fd = open("filex.txt", O_RDONLY);
-	while (i++ < 9)
+	while (i++ < 10)
 	{
 		str = get_next_line(fd);
 		printf("%s\n", str);
